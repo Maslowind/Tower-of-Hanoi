b4cb896e9da78e0f6c19e33692fa3ada75f76f5c
maslowind <bart1885@gmail.com>
2020-03-17 12:58:46 +0200

    time with splice

--- index.js
+++ index.js
@@ -1,52 +1,81 @@
-const express = require ('express');
-let app = express ();
+const express = require('express');
+let app = express();
 
-const jsonHelper = require ('./Services/jsonHelper');
+const jsonHelper = require('./Services/jsonHelper');
 
-let A = [] ;
-let B = [] ;
-let C = [] ;
+let A = [];
+let B = [];
+let C = [];
 
-getArray =  function (count) {
-    for( let i = 1; i <= count; i++) 
-        A.push (i);
+prepareDataset = function (count) {
+  for (let i = 1; i <= count; i++)
+    A.push(i);
+  A.reverse();
+  B = [];
+  C = [];
 };
 
-getOutput = function () {
-    console.log ( "A :", A);
-    console.log ( "B :", B);
-    console.log ( "C :", C);
-    console.log ();    
+printAll = function () {
+  console.log("A :", A);
+  console.log("B :", B);
+  console.log("C :", C);
+  console.log();
 }
 
 
-getTower =  function (a, from, middle, to) {
-    let el;
-    if (a == 1) {
-      getOutput ();
-      el = from.pop ();
-      to.push (el);
-      return;
-    }
-    else {
-      getTower (a - 1, from, to, middle);
-      getOutput ();
-      el = from.pop ();
-      to.push (el);
-      getTower (a - 1, middle, from, to);
-    }
-  };
+towerWithPushPop = function (a, from, middle, to) {
+  let el;
+  if (a == 1) {
+    el = from.pop();
+    to.push(el);
+    return;
+  }
+  else {
+    towerWithPushPop(a - 1, from, to, middle);
+    el = from.pop();
+    to.push(el);
+    towerWithPushPop(a - 1, middle, from, to);
+  }
+};
 
-app.get('/', function(req, res) {    
-    const count = jsonHelper.getJsonData ();
-    getArray (count);    
-    getTower (count, A, B, C);    
-    getOutput ();
-    res.send("Hey! Look at the console! =)");    
+towerWithSplice = function (a, from, middle, to) {
+  let el;
+  if (a == 1) {
+    el = from.splice(from.length - 1, 1)
+    to.concat(el);
+    to.splice(to.length, 0, el[0])
+    return;
+  }
+  else {
+    towerWithSplice(a - 1, from, to, middle);
+    el = from.splice(from.length - 1, 1)
+    to.splice(to.length, 0, el[0])
+    towerWithSplice(a - 1, middle, from, to);
+  }
+};
+
+app.get('/', function (req, res) {
+  const count = jsonHelper.getJsonData();
+
+  for (let i = 0; i < 25; i++) {
+    prepareDataset(count);
+    const startWithPushPop = new Date().getTime();
+    towerWithPushPop(count, A, B, C);
+    const endWithPushPop = new Date().getTime();
+    console.log('With PushPop:', endWithPushPop - startWithPushPop, 'ms');
+
+    prepareDataset(count);
+    const startWithSplice = new Date().getTime();
+    towerWithSplice(count, A, B, C);
+    const endWithSplice = new Date().getTime();
+    console.log('With Splice:', endWithSplice - startWithSplice, 'ms');
+  }
+
+  res.send("Hey! Look at the console! =)");
 });
-    
 
- let server = app.listen(3000, function () {
-      console.log ('Server is running!');
-  });
- 
+
+let server = app.listen(3000, function () {
+  console.log('Server is running!');
+});
+
